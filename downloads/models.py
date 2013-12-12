5226a26cd6afd4ed872fbc1d591608b1cea30300
pyprism <areose21@gmail.com>
2013-12-12 09:25:26 +0600

    update

--- downloads/models.py
+++ downloads/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+	userName = models.CharField(max_length=30)
+	userEmail = models.CharField(max_length=50)
+	password = models.CharField(max_length=50)
+
+class File(models.Model):
+	fileName = models.CharField(max_length=300)
