45b297d708cc161792f31d88096ee6796810d62f
pyprism <areose21@gmail.com>
2013-12-21 07:25:43 +0600

    update

--- music/admin.py
+++ music/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from music.models import Playlist
 # Register your models here.
+
+class PlaylistAdmin(admin.ModelAdmin):
+	list_display = ('songName','favorite')
+	search_fields = ('songName' ,)
+
+admin.site.register(Playlist,PlaylistAdmin)
--- music/models.py
+++ music/models.py
@@ -4,6 +4,12 @@ from django.contrib.auth.models import User
 
 class Playlist(models.Model):
 	user = models.ForeignKey(User)
-	url = models.URLField()
-	songName = models.CharField(max_length=128, verbose_name = "Song Name")
+	songName = models.CharField(max_length=128, verbose_name = "Song's Name")
+	url = models.URLField(verbose_name = "Song's URL")
 	favorite = models.BooleanField(default=False)
+
+	class Meta:
+		verbose_name = "Playlist"
+
+	def __unicode__(self):
+		return self.songName
